929bf04bcf085ad22887dafeaf89a8551085e876
Nikita735 <kononovnikita29@gmail.com>
2020-08-07 20:10:36 +0700

    1 commit

--- news/admin.py
+++ news/admin.py
@@ -1,14 +1,25 @@
 from django.contrib import admin
+from django.utils.safestring import mark_safe
 
 from .models import News, Category
 
 
 class NewsAdmin(admin.ModelAdmin):
-    list_display = ('id', 'title', 'category', 'creater_at', 'is_published')
+    list_display = ('id', 'title', 'category', 'creater_at', 'is_published', 'get_photo')
     list_display_links = ('id', 'title')
     search_fields = ('title', 'content')
     list_editable = ('is_published',)
     list_filter = ('is_published', 'category')
+    fields = ('title', 'category', 'content', 'photo', 'get_photo', 'views', 'is_published', 'creater_at')
+    readonly_fields = ('get_photo', 'views', 'is_published', 'creater_at')
+    save_on_top = True
+
+    def get_photo(self, obj):
+        if obj.photo:
+            return mark_safe(f'<img scr="{obj.photo.url}" widt="75">')
+        else:
+            return '-'
+    get_photo.short_description = 'Миниатюра'
 
 class CategoryAdmin(admin.ModelAdmin):
         list_display = ('id', 'title')
@@ -17,3 +28,6 @@ class CategoryAdmin(admin.ModelAdmin):
 
 admin.site.register(News, NewsAdmin)
 admin.site.register(Category, CategoryAdmin)
+
+admin.site.site_title = 'Управелине новостями'
+admin.site.site_header = 'Управелине новостями'
